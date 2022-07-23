26c7b3ca12948b09088e2f987b23ba7ec4751cbb
Gerseneck <wang14240@gmail.com>
2022-07-23 16:08:46 -0400

    Init Commit

---
+++ src/main/java/io/github/com/gerseneck/gersenecksmpplugin/GersenecksmppluginMain.java
@@ -0,0 +1,17 @@
+package io.github.com.gerseneck.gersenecksmpplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GersenecksmppluginMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
