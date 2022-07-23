feaed519032e745e3cf63fefcc43ba3f0e85f966
Gerseneck <wang14240@gmail.com>
2022-07-23 19:57:20 -0400

    fixed CheckDurability

--- src/main/java/io/github/com/gerseneck/gersenecksmpplugin/commands/CheckDurability.java
+++ src/main/java/io/github/com/gerseneck/gersenecksmpplugin/commands/CheckDurability.java
@@ -3,9 +3,8 @@ package io.github.com.gerseneck.gersenecksmpplugin.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Damageable;
+import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
@@ -25,18 +24,21 @@ public class CheckDurability implements CommandExecutor {
             PlayerInventory inv = ((Player) sender).getInventory();
             ItemStack im = null;
             if (args[0].equalsIgnoreCase("hand")) { im = inv.getItemInHand(); }
-            if (args[0].equalsIgnoreCase("offhand")) { im = inv.getItemInOffHand(); }
-            if (args[0].equalsIgnoreCase("helmet")) { im = inv.getHelmet(); }
-            if (args[0].equalsIgnoreCase("chestplate")) { im = inv.getChestplate(); }
-            if (args[0].equalsIgnoreCase("leggings")) { im = inv.getLeggings(); }
-            if (args[0].equalsIgnoreCase("boots")) { im = inv.getBoots(); }
+            else if (args[0].equalsIgnoreCase("offhand")) { im = inv.getItemInOffHand(); }
+            else if (args[0].equalsIgnoreCase("helmet")) { im = inv.getHelmet(); }
+            else if (args[0].equalsIgnoreCase("chestplate")) { im = inv.getChestplate(); }
+            else if (args[0].equalsIgnoreCase("leggings")) { im = inv.getLeggings(); }
+            else if (args[0].equalsIgnoreCase("boots")) { im = inv.getBoots(); }
+            else {
+                sender.sendMessage("§4Specify a slot to check! HAND, OFFHAND, HELMET, CHESTPLATE, LEGGINGS, BOOTS.");
+                return true;
+            }
             if (!(im.getItemMeta() instanceof Damageable)) {
                 sender.sendMessage("§3This Item does not have any durability.");
                 return true;
             }
-            int health = (int) ((Damageable) im.getItemMeta()).getHealth();
-            int totalHealth = (int) ((Damageable) im.getItemMeta()).getMaxHealth();
-            sender.sendMessage(String.format("Item %s has %d Durability left. Total Durability %d", im.getItemMeta().getDisplayName(), health, totalHealth));
+            int health = (int) ((Damageable) im.getItemMeta()).getDamage();
+            sender.sendMessage(String.format("Item %s has %d Durability left.", im.getItemMeta().getDisplayName(), health));
         }
         return false;
     }
